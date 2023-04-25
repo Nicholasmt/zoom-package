02aae2699924591c15df3630694fed706776836f
mba <Nicholasmt>
2023-04-25 18:52:24 +0100

    updated service provider

--- src/Providers/ZoomProvider.php
+++ src/Providers/ZoomProvider.php
@@ -24,7 +24,7 @@ class ZoomProvider extends ServiceProvider
     public function boot()
     {
 
-        $this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
+        // $this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
         $this->publishes([ __DIR__.'/../Controllers' => app_path('Http/nicholasmt/'),], 'library-controller');
         // $this->info('packed publshed');
         $this->publishes([ __DIR__.'/../php-jwt-master' => app_path('Http/nicholasmt/php-jwt-master/'),], 'jwt-master');
