877c475ecaae1ffdf18dccd3a00773e6c39cb662
mba <Nicholasmt>
2023-05-18 15:22:04 +0100

    migrated from JWT  to Server to server zoom api app as recommended by zoom

--- src/Zoom.php
+++ src/Zoom.php
@@ -1,37 +1,72 @@
 <?php 
  
- namespace Nicholasmt\ZoomLibrary;
+ namespace App\Http\Library;
  
 //Include Firebase Library and Dependencies
-// require __DIR__ . '../Models/Database.php';
 require_once 'php-jwt-master/src/BeforeValidException.php';
 require_once 'php-jwt-master/src/ExpiredException.php';
 require_once 'php-jwt-master/src/SignatureInvalidException.php';
 require_once 'php-jwt-master/src/JWT.php';
 
 use \Firebase\JWT\JWT;
-use Illuminate\Support\Facades\Facade;
 
-class Zoom extends Facade
+
+class Zoom
 {
-	 
-	// private $zoom_api_key = ;
-	// private $zoom_api_secret = 'your api secret key';	
-	
-	//function to generate JWT
-	private function generateJWTKey() {
-		$key = env('ZOOM_API_KEY');
-		$secret = env('ZOOM_API_SECRET');
-		$token = array(
-			"iss" => $key,
-			"exp" => time() + 3600 //60 seconds as suggested
+ 
+    //function to generate Token
+	private function generateToken() 
+	{
+		$key = $this->zoom_api_key;
+
+		$secret = $this->zoom_api_secret;
+		 $token = array(
+		 	"iss" => $key,
+		 	"exp" => time() + 3600 //60 seconds as suggested
 		);
-		return JWT::encode( $token, $secret );
+
+		    // $account_ID = "vQ9jdIL0RB-LpdysKZOmYw";
+			// $client_ID =  'xK8i2MNFQm28PZOcsNXimQ';
+			// $client_secret = 'fhfv3yTn3Mg0Ae9qu7po7uWKHWCpUa7q';
+			$scope = array('meeting:write:admin',
+			               'meeting:write'
+	                      );
+
+			$account_ID	    = env('ACCOUNT_ID');
+			$client_ID	    = env('CLIENT_ID');
+			$client_secret	= env('CLIENT_SECRET');
+		 
+		    
+			$url = "https://zoom.us/oauth/token?grant_type=account_credentials&account_id=" . $account_ID;
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_POST, TRUE);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(        
+				'grant_type'    => 'client_credentials',    # https://www.oauth.com/oauth2-servers/access-tokens/client-credentials/        
+				'scope'         =>$scope,  
+			)));
+
+			$headers[] = "Authorization: Basic " . base64_encode($client_ID . ":" . $client_secret);
+			$headers[] = "Content-Type: application/x-www-form-urlencoded";
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+			$data = curl_exec($ch);
+			$auth = json_decode($data, true);
+		    $access_token = $auth['access_token'];
+			 
+             // for JWT
+			// return JWT::encode($access_token, $secret);
+			
+			// for OAuth  access token
+			return $access_token;
+
 	}	
 	
 	//function to create meeting
-    	public  function createMeeting($data = array())
-    	{
+     public function createMeeting($data = array())
+    {
 		$post_time  = $data['start_date'];
 		$start_time = gmdate("Y-m-d\TH:i:s", strtotime($post_time));
 
@@ -67,16 +102,12 @@ class Zoom extends Facade
 	}	
 	
 	//function to send request
-    	protected function sendRequest($data)
-    	{
-
-		//Enter_Your_Email
-		$zoom_email = env('ZOOM_EMAIL');
-		$request_url = "https://api.zoom.us/v2/users/$zoom_email/meetings";
-		
-		$headers = array(
-			"authorization: Bearer ".$this->generateJWTKey(),
-			"content-type: application/json",
+    protected function sendRequest($data)
+    {
+		$request_url = "https://api.zoom.us/v2/users/me/meetings";
+	    $headers = array(
+			"authorization: Bearer ".$this->generateToken(),
+			 "content-type: application/json",
 			"Accept: application/json",
 		);
 		
@@ -84,7 +115,7 @@ class Zoom extends Facade
 		
         	$ch = curl_init();
         	curl_setopt_array($ch, array(
-            	CURLOPT_URL => $request_url,
+            CURLOPT_URL => $request_url,
 	    	CURLOPT_RETURNTRANSFER => true,
 	    	CURLOPT_ENCODING => "",
 	    	CURLOPT_MAXREDIRS => 10,
@@ -93,14 +124,16 @@ class Zoom extends Facade
 	    	CURLOPT_CUSTOMREQUEST => "POST",
 	    	CURLOPT_POSTFIELDS => $postFields,
 	    	CURLOPT_HTTPHEADER => $headers,
+		 
         	));
 
         	$response = curl_exec($ch);
         	$err = curl_error($ch);
         	curl_close($ch);
-        	if (!$response) {
-            		return $err;
-		}
+        	if (!$response) 
+			{
+              return $err;
+		    }
         	return json_decode($response);
 	}
 }
